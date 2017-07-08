83aaa71e6578a429dbfc044d91b0be6a645d3fbc
Ed Sweeney <ed@table.co>
2017-07-08 16:53:36 -0700

    package

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const {h, mount, Component, Text} = require('ink');
+class Counter extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			i: 0
+		};
+	}
+
+	render() {
+		return (
+			<Text green>
+				{this.state.i} tests passed
+			</Text>
+		);
+	}
+
+	componentDidMount() {
+		this.timer = setInterval(() => {
+			this.setState({
+				i: this.state.i + 1
+			});
+		}, 100);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timer);
+	}
+}
+
+mount(<Counter/>, process.stdout);
+
