a4d78f2a26685f69a2bfe36197427904ceccf402
Ed Sweeney <ed@table.co>
2017-07-08 17:34:54 -0700

    commander

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 const {h, mount, Component, Text} = require('ink');
+const program = require('commander');
+
+program
+  .version('0.1.0')
+  .option('-S, --subscription [subscription]', 'Set Subscription ID')
+  .option('-c, --clientId [clientId]', 'set service principal client ID')
+  .option('-s, --secret [secret]', 'set service principal secret')
+  .option('-g, --group [group]', 'set resource group')
+  .parse(process.argv);
+
+program.on('--help', function(){
+  console.log('');
+  console.log('you must set the above params and point to a')
+  console.log('resource group that contains a VNet that has')
+  console.log('subnets, public IPs, load balancers, and NSGs')
+  console.log('');
+});
+
+if (!program.subscription || !program.clientId || !program.secret) {
+  program.help()
+}
+
 class Counter extends Component {
 	constructor() {
 		super();
@@ -28,6 +50,5 @@ class Counter extends Component {
 		clearInterval(this.timer);
 	}
 }
-
 mount(<Counter/>, process.stdout);
 
