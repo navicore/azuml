960dba04933067327950d21f2815bda01e510a20
Ed Sweeney <ed@onextent.com>
2017-07-19 20:31:40 -0700

    util

--- src/util.js
+++ src/util.js
@@ -1,10 +1,12 @@
 const sortSubnets = (subnets) => {
+  //todo: parameterize on cli score rules
   const score = (subnet) => {
     if (subnet.name.includes('bastion')) return 0
     if (subnet.name.includes('public')) return 1
-    if (subnet.name.includes('database')) return 4
+    if (subnet.name.includes('database')) return 3
+    if (subnet.name.includes('control')) return 4
     if (subnet.name.includes('cicd')) return 5
-    return 3
+    return 2
   }
   return subnets.sort((a, b) => {
     let s1 = score(a)
