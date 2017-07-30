c619c00a767c77cd861d2276c63305d51a305416
Ed Sweeney <ed@onextent.com>
2017-07-30 10:28:49 -0700

    tweak sort to put control before db

--- src/util.js
+++ src/util.js
@@ -3,8 +3,8 @@ const sortSubnets = subnets => {
   const score = subnet => {
     if (subnet.name.includes("bastion")) return 0;
     if (subnet.name.includes("public")) return 1;
-    if (subnet.name.includes("database")) return 3;
-    if (subnet.name.includes("control")) return 4;
+    if (subnet.name.includes("control")) return 3;
+    if (subnet.name.includes("database")) return 4;
     if (subnet.name.includes("cicd")) return 5;
     return 2;
   };
